edec9ffa50f6efe4cf0bccfd39ddf0a1f5d4fba1
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-02-25 15:48:00 -0800

    test: add getUserAgent.bundle.spec.cjs

---
+++ src/getUserAgent.bundle.spec.cjs
@@ -0,0 +1,25 @@
+const { strictEqual } = require("node:assert");
+const { existsSync, readdirSync } = require("node:fs");
+const { join } = require("node:path");
+const { describe, it } = require("node:test");
+
+const buildDir = join(__dirname, "__fixtures__", "build");
+const files = readdirSync(buildDir).filter((fileName) =>
+  fileName.endsWith(".cjs"),
+);
+
+describe("getUserAgent returns values from bundles", async () => {
+  const userAgentPrefix = existsSync(
+    join(process.cwd(), "node_modules", "typescript", "package.json"),
+  )
+    ? "md/tsc"
+    : "api/s3";
+
+  for (const file of files) {
+    it(`${file}: getUserAgent returns '${userAgentPrefix}'`, async () => {
+      const { getUserAgent } = require(join(buildDir, file));
+      const userAgent = await getUserAgent();
+      strictEqual(userAgent[5][0], userAgentPrefix);
+    });
+  }
+});
