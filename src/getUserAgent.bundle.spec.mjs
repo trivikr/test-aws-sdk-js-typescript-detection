809d056c8426ab6b5f31286848581d0650fa9843
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-02-25 14:47:29 -0800

    chore: add getUserAgent.bundle.spec.mjs

---
+++ src/getUserAgent.bundle.spec.mjs
@@ -0,0 +1,23 @@
+import { strictEqual } from "node:assert";
+import { existsSync, readdirSync } from "node:fs";
+import { join } from "node:path";
+import { describe, it } from "node:test";
+
+const buildDir = join(import.meta.dirname, "__fixtures__", "build");
+const files = readdirSync(buildDir);
+
+describe("getUserAgent returns values from bundles", async () => {
+  const userAgentPrefix = existsSync(
+    join(process.cwd(), "node_modules", "typescript", "package.json"),
+  )
+    ? "md/tsc"
+    : "api/s3";
+
+  for (const file of files) {
+    it(`${file}: getUserAgent returns '${userAgentPrefix}'`, async () => {
+      const { getUserAgent } = await import(join(buildDir, file));
+      const userAgent = await getUserAgent();
+      strictEqual(userAgent[5][0], userAgentPrefix);
+    });
+  }
+});
