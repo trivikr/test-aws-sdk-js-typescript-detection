dbbfb84deb6d013d2c850114398779687b8bd374
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2026-02-25 13:02:57 -0800

    test: check for typescript when installed

--- src/getUserAgent.spec.cjs
+++ src/getUserAgent.spec.cjs
@@ -1,5 +1,7 @@
-const { test } = require("node:test");
 const { strictEqual } = require("node:assert");
+const { existsSync } = require("node:fs");
+const { join } = require("node:path");
+const { test } = require("node:test");
 const { getUserAgent } = require("./getUserAgent.cjs");
 
 test("getUserAgent returns values", async () => {
@@ -8,4 +10,13 @@ test("getUserAgent returns values", async () => {
   strictEqual(userAgent[1][0], "ua");
   strictEqual(userAgent[3][0], "lang/js");
   strictEqual(userAgent[4][0], "md/nodejs");
+  if (
+    existsSync(
+      join(process.cwd(), "node_modules", "typescript", "package.json"),
+    )
+  ) {
+    strictEqual(userAgent[5][0], "md/tsc");
+  } else {
+    strictEqual(userAgent[5][0], "api/s3");
+  }
 });
