41a3f9f5d870ca4913ebc06dd21034a82b5ac70c
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-02-25 16:01:37 -0800

    chore(ci): add GitHub workflow to test MJS (#2)

---
+++ src/getUserAgent.spec.mjs
@@ -0,0 +1,34 @@
+import { strictEqual } from "node:assert";
+import { existsSync } from "node:fs";
+import { join } from "node:path";
+import { describe, it } from "node:test";
+import { getUserAgent } from "./getUserAgent.mjs";
+
+describe("getUserAgent returns values", async () => {
+  const userAgent = await getUserAgent();
+
+  const prefixToTest = [
+    [0, "aws-sdk-js"],
+    [1, "ua"],
+    // [2, "os/darwin"], // Skipping as this changes based on OS platform
+    [3, "lang/js"],
+    [4, "md/nodejs"],
+  ];
+
+  if (
+    existsSync(
+      join(process.cwd(), "node_modules", "typescript", "package.json"),
+    )
+  ) {
+    prefixToTest.push([5, "md/tsc"]);
+    prefixToTest.push([6, "api/s3"]);
+  } else {
+    prefixToTest.push([5, "api/s3"]);
+  }
+
+  for (const [index, expected] of prefixToTest) {
+    it(`should return '${expected}' at index [${index}][0]`, () => {
+      strictEqual(userAgent[index][0], expected);
+    });
+  }
+});
