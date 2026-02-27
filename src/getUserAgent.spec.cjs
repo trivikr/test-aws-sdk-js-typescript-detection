d56f0ef5d0b153d3ba15fdd6f018cf42a00a2c2d
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2026-02-27 14:34:05 -0800

    test(ci): rename TS_VERSION TO TSC_VERSION (#10)

--- src/getUserAgent.spec.cjs
+++ src/getUserAgent.spec.cjs
@@ -5,9 +5,9 @@ const { getUserAgent } = require("./getUserAgent.cjs");
 it("getUserAgent", async () => {
   const userAgent = await getUserAgent();
 
-  if (process.env.TS_VERSION) {
+  if (process.env.TSC_VERSION) {
     strictEqual(userAgent[5][0], "md/tsc");
-    strictEqual(userAgent[5][1], process.env.TS_VERSION);
+    strictEqual(userAgent[5][1], process.env.TSC_VERSION);
   } else {
     strictEqual(userAgent[5][0], "api/s3");
   }
